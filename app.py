e9be47994ad7fb8deefcfa55c020404ee6a959b3
Maximilian Spurman <maximilianspurman@MacBook-Air-von-Maximilian.local>
2026-03-25 14:50:34 +0100

    Add live password strength analysis with improved UI

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    result = None
-    if request.method == "POST":
-        password = request.form.get("password")
-        result = f"Dein Passwort ist: {password}"
-    return render_template("index.html", result=result)
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
